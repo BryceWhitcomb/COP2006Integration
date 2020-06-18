d1874701de12c6cb489adb2dfc7dffa76b387a2c
BryceWhitcomb <54415364+BryceWhitcomb@users.noreply.github.com>
2020-06-17 23:22:49 -0400

    Create Constructors.java

---
+++ Constructors.java
@@ -0,0 +1,17 @@
+// class Constructors {
+//     int sampleArray[];
+//     int totArray;
+//     public int smolConstructors(int sampleArray[], int totArray) {
+//         int bigg;
+//         for(int smallCount = 0; smallCount < totArray; smallCount++ ){
+//             for (int smallCount2 = smallCount + 1; smallCount2 < totArray; smallCount2++){
+//                 if(sampleArray[smallCount] > sampleArray[smallCount2]){
+//                     bigg = sampleArray[smallCount];
+//                     sampleArray[smallCount] = sampleArray[smallCount2];
+//                     sampleArray[smallCount2] = bigg;
+//                 }
+//             }
+//         }
+//         return sampleArray[0];
+//     }
+// }
