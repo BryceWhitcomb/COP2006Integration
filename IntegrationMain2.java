94b201946ac521777b2484a59daa618cc69ea5d4
BryceWhitcomb <54415364+BryceWhitcomb@users.noreply.github.com>
2020-06-17 23:21:57 -0400

    Update IntegrationMain2.java

--- IntegrationMain2.java
+++ IntegrationMain2.java
@@ -1,178 +1,238 @@
 //Bryce Whitcomb
 //June 1, 2020
-//Bookshelf Calculator
-//This program does not account for input errors
-//This program calculates the number of books that can be stored on a bookshelf.
+//Integration Project
+//Description: Due to me having absolutely no imagination, ...
+//this program will demonstrate all of the code we have reviewed ...
+//in this course in a cohesive manner, but not have a central purpose.
 
 //Below are some definitions for keywords relating to this program
 //Variable: A variable is a type of data that is stored as something with a different name.
 //Scope: A scope is the association of a variable to its data.
 //Method Call: A method call is a method name 
+//Operator precedence: The order in which operations will take place in an expression.
+//Inheritance: The ability of a child class to access the fields of its parent class.
+//Polymorphism: Referencing a child class using a parent class.
+//Data types: Types of vaiables in java.
 
 package IntegrationProject;
-import java.util.Scanner;
-import static java.lang.Math.*;
-import java.util.Random;
-import java.util.ArrayList;
+//imports everything within the java.util class.
+import java.util.*;
+public class IntegrationMain {
 
-public class IntegrationMain2 {
-
-    //This method is called to compare the length of two book titles.
-    //The method is titles bookLength and its argument will be the values inserted into the parameters.
-    //The header for this method is the int, determining what datatype will be returned.
-    //The parameters for this method are the ints within the parentheses that will be replaced by a later argument.
-    static int bookLength(int bookName, int bookName2) {
-        int bookNameLength = max(bookName, bookName2);
-        return bookNameLength;
+    //Method with arguments and return values
+    //Header: The first line in a method, which declares the type and name.
+    //Parameter: The argument within the method's parentheses.
+    static int addMethod(int num1, int num2){
+        return num1 + num2;
     }
 
-    static String facts() {
-        String fact = "The next section will give you data on the books on your shelf.";
-        return fact;
+    //Method overloading
+    //Method overloading occurs when the same method can have 2 different arguments.
+    static double addMethod(double num1, double num2){
+        return num1 + num2;
     }
 
-    static String goodbye() {
-        String l8r = "Thanks for using this program!";
-        return l8r;
-    }
+    
+    
+    public static void main(String[] args) {
+        
+        //This section initializes the scanner and random classes.
+        //The Scanner class is now called with input
+        //The Random class is now called with random
+        Scanner input = new Scanner(System.in);
+        Random random = new Random();
 
-    public static void main( String[] args) {
-
-         Scanner input = new Scanner(System.in);
-
-         Random random = new Random();
-
-        //This is a greeting using a print statement.
-        //It is the first thing the user will see.
-        System.out.println("This program calculates the total shelfspace of a bookshelf.");
-        System.out.println("Additionally it will help inventory the books you have and choose books to read!\n");
-
-        System.out.println("How many shelves does the bookshelf have?");
-        int shelves = input.nextInt();
-
-        //This is a double variable used to determine the length of each booksehelf.
-        //A double is a variable that always contains a number, including its decimal value.
-        //The deciaml of a double ranges from 0 to 15 digits.
-        System.out.println("How long is the length of one shelf in centimeters?");
-        double length = input.nextDouble();
-
-        System.out.println("What is the length of one of your books in centimeters?");
-        double bookLength = input.nextDouble();
-
-        //This section calculates how many copies of the book can be stored on the shelf.
-        //This section also casts the bookLength and totalLength variables from doubles to integers.
-        //casting is the process of changing a variable from one type to another.
-        double totalLength = length * shelves;
-        int bookAmount = (int) (totalLength / bookLength);
-        System.out.println("You can fit approximately " + bookAmount + " books on the bookshelf.\n");
-
-        //This section will determine how many books the user has and what genre they belong to.
-        //The while statement below executes as long as the user does not input "stop".
-        //While loops only execute while the argument is true.
-        System.out.println("Please enter the name of each non-fiction book you have.\n"
-        + "Enter the books one at a time and type 'stop' when you are finished.");
-        int nonFicCount = 0;
-        String nonFicString = "x";
-        ArrayList<String> nonFicList = new ArrayList<String>();
-        while (!nonFicString.equals("stop")) {
-            nonFicString = input.nextLine();
-            nonFicList.add(nonFicString);
-            nonFicCount = nonFicCount + 1;
+        //This is the greeting, the first message the user will see.
+        //The println function will print the contents of the function on the next line.
+        System.out.println("This program will demonstrate a wide array of what Java is capable of.");
+        
+        //This section demonstrates how while loops work, as well as some other functions.
+        System.out.println("First this program will demonstrate how while loops can work.");
+        System.out.println("This while loop will count all odd or even numbers in a range of your choice.");
+        System.out.println("Enter the number whole you would like to start at.");
+        //An int is a variable that stores the intager part of a number.
+        //the nextInt function will take the value of the next intager input by the user.
+        int lowNum = input.nextInt();
+        //final
+        //A final will cause a variable to keep its value permanently
+        final int initialLowNum = lowNum;
+        System.out.println("Next enter the number whole you would like to end at.");
+        int highNum = input.nextInt();
+        final int initialHighNum = highNum;
+        System.out.println("All odd or even numbers in an ascending order:");
+        //while loops will loop while the argument within them is true.
+        //A relational operator compares two values, usually with < or >
+        while(lowNum <= highNum){
+            //+ in a print statement concatinates the two things on each side of it
+            System.out.print(lowNum + " ");
+            //+= will add the sum of the numbers to the right of it to the variable to the left of it.
+            lowNum += 2;
+        }
+        System.out.println("\nAll of the numbers within this range in ascending order:");
+        //This line will reset lowNum to its initial value.
+        lowNum = initialLowNum;
+        while(lowNum <= highNum){
+            System.out.print(lowNum + " ");
+            //++ increases the value of the variable next to it by 1
+            lowNum++;
         }
 
-        System.out.println("\nPlease enter the name of each fiction book you have.\n"
-        + "Enter the books one at a time and type 'stop' when you are finished.");
-        int FicCount = 0;
-        String FicString = "x";
-        ArrayList<String> FicList = new ArrayList<String>();
-        while (!FicString.equals("stop")) {
-            FicString = input.nextLine();
-            FicList.add(FicString);
-            FicCount = FicCount + 1;
+        System.out.println("\nJava can also count in a decending order:");
+        lowNum = initialLowNum;
+        while(highNum >= lowNum){
+            System.out.print(highNum + " ");
+            //-= subtracts the sum to the right of it by the variable to the left of it.
+            highNum -= 2;
         }
-
-        //This section will determine what genre the user wants to read
-        //This section uses a switch statement, which has a predetermined number of outputs, based on what case is chosen
-        //If no cases are activated, the switch will use the default output.
-        //The break statement will end any loop, including a switch.
-        System.out.println("\nWhat genre of book do you want to read?");
-        System.out.println("Enter 1 for fiction.");
-        System.out.println("Enter 2 for non-fiction.");;
-        System.out.println("Enter 3 if you do not want to read anything.");
-        int genre = input.nextInt();
-        String genreString;
-        switch(genre) {
-            case 1: genreString = "Fiction";
-                break;
-            case 2: genreString = "Non-Fiction";
-                break;
-            case 3: genreString = "None";
-                break;
-            default: genreString = "Invalid input";
-                break;
+        System.out.println("\nAll of the numbers within this range in descending order:");
+        highNum = initialHighNum;
+        while(highNum >= lowNum){
+            System.out.print(highNum + " ");
+            //-- reduces the value of a variable next to it by 1
+            highNum--;
         }
-
-        //This section will reccomend a random book from the requested genre.
-        //The == operator determines if 
-        if (genreString == "None"){
-            System.out.println("There is no book recommendation to make");
-        }
-        if (genreString == "Non-Fiction"){
-            int nonFicRand = random.nextInt(nonFicCount - 1) + 1;
-            System.out.println("You should read " + nonFicList.get(nonFicRand));
-        }
-        if (genreString == "Fiction"){
-            int FicRand = random.nextInt(FicCount - 1) + 1;
-            System.out.println("You should read " + nonFicList.get(FicRand));
-        }
-
-        //A for loop will run a set number of times.
-        //The ++ operator adds 1 to the int each time it loops.
-        //The compareTo method compares the length of 2 strings and returns the difference of them.
-        //The || is a conditional operator for or in java.
-        int nonFicBook = 0;
-        int nonBookCount = 1;
-        int nonBookCount2 = 0;
-        int nonFicListSize = nonFicList.size();
+        System.out.println("\nThis section will divide the larger input by the smaller input\n" +
+        "until it reaches a non divisible number, in which case a remainder is printed.");
+        highNum = initialHighNum;
+        int joyDivision = 0;
+        int joyCount = 0;
+        //The conditional operator is the ternary operator ?, which makes an if, then, else.
+        //A do-while loop will always run once, then will run while it remains true.
         do {
-            String firstBook = nonFicList.get(nonFicBook + nonBookCount2);
-            String secondBook = nonFicList.get(nonFicBook + nonBookCount);
-            int bookDiff = firstBook.compareTo(secondBook);
-            if (bookDiff >= 0) {
-                nonBookCount = nonBookCount + nonBookCount2 + 1;
+            //% is the remainder operator, producing the remainder of one number that was divided by another
+            joyDivision = (highNum % lowNum);
+            joyCount++;
+            //An if statement will only run if its argument is true
+            if(joyDivision == highNum){
+                //A break will end the current loop
+                break;
             }
-            else{
-                nonBookCount2 = nonBookCount2 + nonBookCount + 1;
+            //== is the equality operator and returns true if the argument is equal on each side
+            if(lowNum == 1){
+                joyDivision = 0;
+                break;
             }
-        }while (nonBookCount2 < nonFicListSize || nonBookCount < nonFicListSize);
-        //The max method comes form the Math class, which is imported at the start.
-        int longNonTitle = max(nonBookCount, nonBookCount2);
-        System.out.println("The non-fiction book with the longest title is " + longNonTitle + "characters!");
-
-        int ficBook = 0;
-        int ficBookCount = 1;
-        int ficBookCount2 = 0;
-        int ficListSize = FicList.size();
-        do {
-            String thirdBook = FicList.get(ficBook + ficBookCount2);
-            String fourthBook = FicList.get(ficBook + ficBookCount);
-            int bookDiff2 = thirdBook.compareTo(fourthBook);
-            if (bookDiff2 >= 0){
-                ficBookCount = ficBookCount + ficBookCount2 + 1;
+            if(lowNum == 0){
+                joyDivision = 0;
             }
-            else{
-                ficBookCount2 = ficBookCount2 + ficBookCount + 1;
-            }
-        }while (ficBookCount2 < ficListSize || ficBookCount < ficListSize);
-        int longTitle = max(ficBookCount, ficBookCount2);
-        System.out.println("The fiction book with the longest title is " + longTitle + "characters!");
+            // / is the division operator
+            highNum = highNum / lowNum;
+        }while(joyDivision == 0);
+        System.out.println("The larger number was divided " + joyCount +
+        " times the smaller number and the remainder was " + joyDivision);
+        //Type casting is the assigment of one type of data to another
+        //A double is a datatype containing a decimal.
+        double doubleHighNum = initialHighNum;
+        double doubleLowNum = initialLowNum;
+        //* is the multiplication operator
+        double visionOfDivision = doubleHighNum / (doubleLowNum * joyCount);
+        System.out.println("The larger number divided by " + joyCount + 
+        " times the smaller number is " + visionOfDivision);
+        //+ is the addition operator
+        System.out.println("Other java operaters include addition and subtraction.\n" + 
+        "The larger number subtracted by the smaller number is: " + (initialHighNum -initialLowNum));
+        //- is the subtraction operator
+        System.out.println("The larger number added to the smaller number is: " + (initialHighNum + initialLowNum));
+        //The random class will produce a random number within the assigned range
+        int rangeNum = initialHighNum - initialLowNum;
+        int randNum = (random.nextInt(rangeNum)) + initialLowNum;
+        System.out.println("A random intager in the rang of the two entered numbers is: " +
+        randNum);
+        double doubleRandNum = randNum;
+        System.out.println("The square root of the random intager is " + Math.sqrt(doubleRandNum));
 
-        int longestBook = bookLength(longNonTitle, longTitle);
+        System.out.println("This next section will show how strings work.");
+        System.out.println("Please enter your favorite animal.");
+        input.nextLine();
+        //A string is a variable containing a sequence of characters
+        String favAnimal = input.nextLine();
+        //An if-else statement is the same as a ternary construct
+        //|| is the logical or operator
+        if(favAnimal.equals("bear")||favAnimal.equals("bears")){
+            System.out.println("That is my favorite too!");
+        }else{
+            System.out.println("That is a cool animal!");
+        }
+        System.out.println("With a string, each character can be printed individually.");
+        int count;
+        //A for loop will run for a set number of times
+        //The length() method will return the character length of a given string 
+        for(count = 0; count < favAnimal.length(); count++){
+            //charAt
+            System.out.print(favAnimal.charAt(count) + " ");
+        }
+        System.out.println("\nEnter your favorite month as an intager.");
+        int month = input.nextInt();
+        String monthS;
+        //A switch statement tests a variable against multiple possible inputs, in order to determine the output
+        switch (month){
+            case 1: monthS = "January";
+                break;
+            case 2: monthS = "February";
+                break;
+            case 3: monthS = "March";
+                break;
+            case 4: monthS = "April";
+                break;
+            case 5: monthS = "May";
+                break;
+            case 6: monthS ="June";
+                break;
+            case 7: monthS = "July";
+                break;
+            case 8: monthS = "August";
+                break;
+            case 9: monthS = "September";
+                break;
+            case 10: monthS = "October";
+                break;
+            case 11: monthS = "November";
+                break;
+            case 12: monthS = "December";
+                break;
+            default: monthS = "Invalid input";
+                break;
+        }
+        System.out.println("Your favorite things are " + favAnimal + " and the month "+
+        monthS + ".");
+        //The compareTo method compares the length of two strings
+        System.out.println("The difference in characters between these tho things is " +
+        favAnimal.compareTo(monthS));
+        
 
-        System.out.println("The longest book on your bookshelf is" + longestBook + "characters long!!");
+        System.out.println("This next section will use arrays." +
+        "\nPlease enter the size of the array you would like to make.");
+        int funCount = 0;
+        int numFunSize = input.nextInt();
+        System.out.println("Please enter the intagers you would like to put into the array.");
+        //An arraylist is an array with an unspecified number of variables
+        ArrayList<Integer> numbersRFun = new ArrayList<Integer>();
+        while(funCount < numFunSize){
+            numbersRFun.add(input.nextInt());
+            funCount++;
+        }
+        int numCount = 1;
+        //The enhanced for loop uses a : and functions similarly to a normal for loop
+        for(Integer arrayNum : numbersRFun){
+            System.out.println(numCount + ": " + arrayNum);
+            numCount++;
+        }
+        //finding the smallest number in an array using 
+        //System.out.println("Next a preset array will be sorted to output the lowest number in it");
+        //int sampleArray[] = {3,5,1,2,6};
+        //Constructors arrayConstruct = new Constructors(sampleArray, 5);
+        //System.out.println("The lowest number is: " + arrayConstruct.smolConstructors());
 
-        goodbye();
-        //This stops the scanner from looking for any inputs.
+        System.out.println("Method overloading can cause a method to output different" +
+        " things based on what is input.\nIf an int is put into this method, an int is output;" +
+        "but if a double if input, a double is output.");
+        //A method call calls a method and uses local inputs to determine what will be output
+        int methodAdd1 = addMethod(3, 4);
+        double methodAdd2 = addMethod(1.7, 2.2);
+        System.out.println("input int: " + methodAdd1);
+        System.out.println("input double: " + methodAdd2);
+        
+        //The close() method will cause java to stop looking for input
         input.close();
-    }
+    }  
 }
